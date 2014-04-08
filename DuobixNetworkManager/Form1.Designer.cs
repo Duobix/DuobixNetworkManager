c5613b965fc87c7654bdcbad7adff6b114cb2364
Duobix <duobix@gmail.com>
2014-04-08 11:30:40 +0200

    Shortcut to network Connections
    Added a button to easy reach network connections.

---
+++ DuobixNetworkManager/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace DuobixNetworkManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.CurrentNetworkTextBox = new System.Windows.Forms.TextBox();
+            this.refreshCurrentNetworkButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(2, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(312, 358);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Show Known WiFi networks";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(2, 412);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(312, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(2, 438);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Delete This Network";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(166, 383);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Show Aviable Wifi networks";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(165, 438);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Connect to this Network";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // CurrentNetworkTextBox
+            // 
+            this.CurrentNetworkTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CurrentNetworkTextBox.Location = new System.Drawing.Point(6, 19);
+            this.CurrentNetworkTextBox.MinimumSize = new System.Drawing.Size(306, 413);
+            this.CurrentNetworkTextBox.Multiline = true;
+            this.CurrentNetworkTextBox.Name = "CurrentNetworkTextBox";
+            this.CurrentNetworkTextBox.ReadOnly = true;
+            this.CurrentNetworkTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.CurrentNetworkTextBox.Size = new System.Drawing.Size(306, 440);
+            this.CurrentNetworkTextBox.TabIndex = 6;
+            // 
+            // refreshCurrentNetworkButton
+            // 
+            this.refreshCurrentNetworkButton.Location = new System.Drawing.Point(231, 465);
+            this.refreshCurrentNetworkButton.Name = "refreshCurrentNetworkButton";
+            this.refreshCurrentNetworkButton.Size = new System.Drawing.Size(81, 23);
+            this.refreshCurrentNetworkButton.TabIndex = 8;
+            this.refreshCurrentNetworkButton.Text = "Refresh";
+            this.refreshCurrentNetworkButton.UseVisualStyleBackColor = true;
+            this.refreshCurrentNetworkButton.Click += new System.EventHandler(this.refreshCurrentNetworkButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.refreshCurrentNetworkButton);
+            this.groupBox1.Controls.Add(this.CurrentNetworkTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.MinimumSize = new System.Drawing.Size(318, 480);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(318, 507);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Current WiFi Network";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(336, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(320, 539);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(106, 470);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Ping This Adress";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 465);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(123, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Network Connections";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 517);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox CurrentNetworkTextBox;
+        private System.Windows.Forms.Button refreshCurrentNetworkButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ DuobixNetworkManager/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace DuobixNetworkManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Duobix Network Manager";
+            shelldoings("netsh", "wlan show interface", null, 3);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            shelldoings("netsh", "wlan show profiles", null, 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            shelldoings("netsh", "wlan delete profile name=", null, 1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            shelldoings("netsh", "wlan show network", null, 1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            shelldoings("netsh", "wlan connect name=", null, 1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            openNetworkConnectionsWindow();
+        }
+        
+        private void openNetworkConnectionsWindow(){
+            ProcessStartInfo startInfo = new ProcessStartInfo("NCPA.cpl");
+            startInfo.UseShellExecute = true;
+            Process.Start(startInfo);
+        }
+        private void shelldoings(string command, string arguments, string networkname, int output)
+        {
+            Process networkshow = new Process();
+            networkshow.StartInfo.FileName = command;
+            networkshow.StartInfo.UseShellExecute = false;
+            networkshow.StartInfo.CreateNoWindow = true;
+            string IDONTWANTTHIS = arguments;
+            if (textBox2.Text != null)
+            {
+                IDONTWANTTHIS += "\"";
+                IDONTWANTTHIS += textBox2.Text;
+                IDONTWANTTHIS += "\"";
+            }
+            networkshow.StartInfo.Arguments = IDONTWANTTHIS;
+            networkshow.StartInfo.RedirectStandardOutput = true;
+            networkshow.Start();
+            textBox2.Text = null;
+            textboxset(output, networkshow.StandardOutput.ReadToEnd());
+            //textBox1.Text = networkshow.StandardOutput.ReadToEnd();
+
+        }
+        private void textboxset(int box, string textStuff)
+        {
+            if (box == 1)
+            {
+                textBox1.Text = textStuff;
+                //textBox1.ScrollBars = ScrollBars.Vertical;
+
+            }else if(box == 2){
+                textBox2.Text = textStuff;
+
+            }else if(box == 3){
+                CurrentNetworkTextBox.Text = textStuff;
+            }
+
+
+        }
+
+        private void refreshCurrentNetworkButton_Click(object sender, EventArgs e)
+        {
+            shelldoings("netsh", "wlan show interface", null, 3);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            shelldoings("ping", "", null, 1);
+        }
+
+        
+
+    }
+}
